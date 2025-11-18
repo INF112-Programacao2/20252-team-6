e166664b639f39df3b4dced32064a486b93991b8
Marcos-Vinicius-Mariano-Dias <marcos.dias@ufv.br>
2025-11-18 00:32:25 -0300

    change_mealPlan feito

--- main.cpp
+++ main.cpp
@@ -5,12 +5,13 @@
 #include "include/Person.hpp"
 
 int main(){
-    Patient Pedro("Pedro", "12345678901", "Rua Presidente Carvalho, 175",
+    Patient Pedro("Pedro Alves", "12345678902", "Rua Presidente Carvalho, 175",
     "Masculino", 12345, "pre-diabetes", "O-", 80, 1.70);
     MealPlan mp("Refrigerante, chocolate", "Sandra", "B12, C", 120, 200, 20, Pedro);
     int id=1;
     mp.register_mealPlan(id);
+    mp.change_mealPlan(id);
     //testando função de buscar id
-    std::cout<<Pedro.searchId(Pedro);
+    std::cout<<Pedro.searchId();
     return 0;
 }
--- src/MealPlan.cpp
+++ src/MealPlan.cpp
@@ -8,6 +8,8 @@ int protein, int carbohydrate, int fat, Patient patient)
 : foodAvoided(foodAvoided), nutricionist(nutricionist), vitamins(vitamins), protein(protein),
 carbohydrate(carbohydrate), fat(fat), patient(patient){}
 
+MealPlan::~MealPlan(){}
+
 void MealPlan::register_mealPlan(int id){
     //conectando ao banco de  dados
     sqlite3* db;
@@ -42,4 +44,149 @@ void MealPlan::register_mealPlan(int id){
     sqlite3_close(db);
 }
 
-MealPlan::~MealPlan(){}
+
+//funções axiliares ao change
+void update_string(int id, const std::string campo, std::string& valor){
+    sqlite3* db = nullptr;
+    sqlite3_stmt* stmt = nullptr;
+    //atualização no database
+    try{
+        int rc = sqlite3_open("database.db", &db);
+        if (rc != SQLITE_OK) {
+            throw std::runtime_error("Erro ao abrir database: " + std::string(sqlite3_errmsg(db)));
+        }
+        std::string sql = "UPDATE PlanoAlimentar SET " + campo + " = ? WHERE id = ?";
+        
+        if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr) != SQLITE_OK) {
+            throw std::runtime_error("Erro ao preparar SQL: " + std::string(sqlite3_errmsg(db)));
+        }
+
+        sqlite3_bind_text(stmt, 1, valor.c_str(), -1, SQLITE_TRANSIENT);
+        sqlite3_bind_int(stmt, 2, id);
+        if (sqlite3_step(stmt) != SQLITE_DONE) {
+            throw std::runtime_error("Erro ao executar UPDATE: " + std::string(sqlite3_errmsg(db)));
+        } else {
+            std::cout << campo << " atualizado com sucesso!" << std::endl;
+        }
+
+        sqlite3_finalize(stmt);
+        sqlite3_close(db);
+    }
+    catch (const std::exception& e) {
+        std::cerr << "ERRO: " << e.what() << std::endl;
+        
+        // Limpeza em caso de erro
+        if (stmt) sqlite3_finalize(stmt);
+        if (db) sqlite3_close(db);
+    }
+}
+
+void update_int(int id, const std::string campo, int& valor){
+    sqlite3* db = nullptr;
+    sqlite3_stmt* stmt = nullptr;
+    //atualização no database
+    try{
+        int rc = sqlite3_open("database.db", &db);
+        if (rc != SQLITE_OK) {
+            throw std::runtime_error("Erro ao abrir database: " + std::string(sqlite3_errmsg(db)));
+        }
+        std::string sql = "UPDATE PlanoAlimentar SET " + campo + " = ? WHERE id = ?";
+        
+        if (sqlite3_prepare_v2(db, sql.c_str(), -1, &stmt, nullptr) != SQLITE_OK) {
+            throw std::runtime_error("Erro ao preparar SQL: " + std::string(sqlite3_errmsg(db)));
+        }
+
+        sqlite3_bind_int(stmt, 1, valor);
+        sqlite3_bind_int(stmt, 2, id);
+        if (sqlite3_step(stmt) != SQLITE_DONE) {
+            throw std::runtime_error("Erro ao executar UPDATE: " + std::string(sqlite3_errmsg(db)));
+        } else {
+            std::cout << campo << " atualizado com sucesso!" << std::endl;
+        }
+
+        sqlite3_finalize(stmt);
+        sqlite3_close(db);
+    }
+    catch (const std::exception& e) {
+        std::cerr << "ERRO: " << e.what() << std::endl;
+        
+        // Limpeza em caso de erro
+        if (stmt) sqlite3_finalize(stmt);
+        if (db) sqlite3_close(db);
+    }
+}
+
+
+void MealPlan::change_mealPlan(int id){
+    //conectando ao banco de  dados
+    sqlite3* db;
+    sqlite3_stmt* stmt;
+
+    int rc=sqlite3_open("database.db",&db);
+    if (rc) {
+        std::cerr << "Erro ao abrir banco: " << sqlite3_errmsg(db) << std::endl;
+    }
+
+    bool continuos = true;
+    while(continuos){
+        std::cout<<"\nDigite o numero referente ao que deseja alterar:\n";
+        int option;
+        std::cout<<"\n1 - Nutricionista";
+        std::cout<<"\n2 - Vitaminas";
+        std::cout<<"\n3 - Proteinas";
+        std::cout<<"\n4 - Carboidrato";
+        std::cout<<"\n5 - Gorduras";
+        std::cout<<"\n6 - Alimentos a evitar";
+        std::cout<<"\n7 - Parar alteracoes\n\n";
+
+        std::cin>>option;
+        std::string valor_string;
+        int valor_int;
+        switch (option)
+        {
+        case 1:
+            std::cout << "Digite o novo nutricionista: ";
+            std::cin.ignore();
+            std::getline(std::cin, valor_string);
+            update_string(id, "Nutricionista", valor_string);
+            nutricionist = valor_string;
+            break;
+        case 2:
+            std::cout << "Digite as vitaminas que quer no seu plano: ";
+            std::cin.ignore();
+            std::getline(std::cin, valor_string);
+            update_string(id, "Vitaminas", valor_string);
+            vitamins = valor_string;
+            break;
+        case 3:
+            std::cout << "Digite a quantidade de proteinas que quer no seu plano: ";
+            std::cin>>valor_int;
+            update_int(id, "Proteinas", valor_int);
+            protein = valor_int;
+            break;
+        case 4:
+            std::cout << "Digite a quantidade de carboidrato que quer no seu plano: ";
+            std::cin>>valor_int;
+            update_int(id, "Carboidratos", valor_int);
+            carbohydrate = valor_int;
+            break;
+        case 5:
+            std::cout << "Digite a quantidade de gordura que quer no seu plano: ";
+            std::cin>>valor_int;
+            update_int(id, "Gorduras", valor_int);
+            fat = valor_int;
+            break;
+        case 6:
+            std::cout << "Digite os alimentos a serem evitados no seu plano: ";
+            std::cin.ignore();
+            std::getline(std::cin, valor_string);
+            update_string(id, "AlimentosEvitados", valor_string);
+            foodAvoided = valor_string;
+            break;
+        default:
+            continuos = false;
+            break;
+        }
+    }
+    sqlite3_close(db);
+}
